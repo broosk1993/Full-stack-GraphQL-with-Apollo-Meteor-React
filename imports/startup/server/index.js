352c93c95938b30c8d999e729097845a3cf07b55
Broosk Hamarash <broosk.edogawa@gmail.com>
2018-04-29 15:11:38 +0300

    GraphQL: Type definition, basic schema, makeExecutableSchema binding with Apollo

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,26 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
+const typeDefs = `
+    type Query {
+        hi: String        
+    }
+`;
 
-createApolloServer({});
+
+const resolvers = { 
+    Query: {
+        hi(){
+            return "Hello Level Up";
+        }
+    }
+}
+
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+
+createApolloServer({ schema });
