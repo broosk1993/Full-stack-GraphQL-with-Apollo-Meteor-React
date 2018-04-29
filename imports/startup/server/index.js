3cf51b09e399d01b6b0c553063156218efb1a329
Broosk Hamarash <broosk.edogawa@gmail.com>
2018-04-29 16:03:48 +0300

    GraphQL Schema Files, Babel config with (babel-plugin-inline-import), array of schemas passed as typeDefs

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,26 +1,2 @@
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
+import './register-api';
 
-const typeDefs = `
-    type Query {
-        hi: String        
-    }
-`;
-
-
-const resolvers = { 
-    Query: {
-        hi(){
-            return "Hello Level Up";
-        }
-    }
-}
-
-
-const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers
-})
-
-
-createApolloServer({ schema });
